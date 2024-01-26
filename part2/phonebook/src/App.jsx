83943971d4ad184b0beb1c7a5387a03c5613da57
impfundev <ilhammp1706@gmail.com>
2024-01-26 19:16:51 +0700

    feat (part-2/phonebook): Fetching data from server with Axios, Effect Hook, and JSON Server.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Filter = ({ handler }) => {
   return (
@@ -55,15 +56,19 @@ const Persons = ({ persons, filter }) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filterText, setFilterText] = useState("");
+
+  useEffect(() => {
+    console.log("effect");
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log("promise fulfilled");
+      setPersons(response.data);
+    });
+  }, []);
+  console.log("render", persons.length, "persons");
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -100,8 +105,6 @@ const App = () => {
   const handleFillterChange = (event) => {
     setFilterText(event.target.value);
   };
-
-  console.log(persons);
 
   return (
     <div>
